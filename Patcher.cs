2d60974ebdf97f511115a63f0e26bf3eb06992d0
jkdev <gpluiy@gmail.com>
2024-06-08 13:30:48 +0200

    Initial commit

---
+++ Patcher.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace PolyPlus {
+    public class Patcher
+    {
+        public static void Load()
+        {
+            Harmony.CreateAndPatchAll(typeof(Patcher));
+            Console.WriteLine("Load!");
+        }
+    }
+}
