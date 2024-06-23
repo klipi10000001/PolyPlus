3fd597be783afba8283b9dea635e405cf0012d2b
jkdev <gpluiy@gmail.com>
2024-06-23 21:07:39 +0200

    Added Embark ability which lets land units to embark when moving on water, without using a port.

--- PolyPlusPatcher.cs
+++ PolyPlusPatcher.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 namespace PolyPlus {
     public class PolyPlusPatcher
     {
-        private static string version = "0.0.8";
-        private static string branch = "main";
+        private static string version = "0.0.9";
+        private static string branch = "waterembark";
         private static int _polyplusAutoidx = 480;
         private static Dictionary<string, int> _polyplusDict = new Dictionary<string, int>();
         public static void Load()
@@ -21,8 +21,8 @@ namespace PolyPlus {
 
         internal static void CreateEnumCaches()
         {
-            //EnumCache<PlayerAbility.Type>.AddMapping("waterembarking", (PlayerAbility.Type)750);
-			//EnumCache<PlayerAbility.Type>.AddMapping("waterembarking", (PlayerAbility.Type)750);
+            EnumCache<PlayerAbility.Type>.AddMapping("waterembark", (PlayerAbility.Type)750);
+			EnumCache<PlayerAbility.Type>.AddMapping("waterembark", (PlayerAbility.Type)750);
             EnumCache<UnitAbility.Type>.AddMapping("polyplusstatic", (UnitAbility.Type)_polyplusAutoidx);
 			EnumCache<UnitAbility.Type>.AddMapping("polyplusstatic", (UnitAbility.Type)_polyplusAutoidx);
             _polyplusDict.Add("polyplusstatic", _polyplusAutoidx);
@@ -63,6 +63,40 @@ namespace PolyPlus {
                 });
                 __instance.Description = string.Format("{0}{1}", unitDescription, unitProgressText);
                 anchoredPosition.x = 48f;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(PathFinder), nameof(PathFinder.IsTileAccessible))]
+        private static void PathFinder_IsTileAccessible(ref bool __result, TileData tile, TileData origin, PathFinderSettings settings)
+	    {
+            if(PlayerExtensions.HasAbility(settings.playerState, (PlayerAbility.Type)750, settings.gameState) && tile.IsWater && (!origin.IsWater || settings.unit != null)){
+                if(settings.allowedTerrain.Contains(tile.terrain) && tile.GetExplored(settings.playerState.Id)){
+                    __result = true;
+                }
+                else{
+                    __result = false;
+                }
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MoveAction), nameof(MoveAction.ExecuteDefault))]
+        private static void MoveAction_ExecuteDefault(MoveAction __instance, GameState gameState)
+	    {
+            UnitState unitState;
+            PlayerState playerState;
+            UnitData unitData;
+            if (gameState.TryGetUnit(__instance.UnitId, out unitState) && gameState.TryGetPlayer(__instance.PlayerId, out playerState) && gameState.GameLogicData.TryGetData(unitState.type, out unitData))
+            {
+                WorldCoordinates worldCoordinates = __instance.Path[0];
+                TileData tile2 = gameState.Map.GetTile(worldCoordinates);
+                tile2.SetUnit(unitState);
+                unitState.coordinates = worldCoordinates;
+                if (!unitData.IsAquatic() && !unitState.HasAbility(UnitAbility.Type.Fly, gameState) && tile2.IsWater && PlayerExtensions.HasAbility(playerState, (PlayerAbility.Type)750, gameState))
+                {
+                    gameState.ActionStack.Add(new EmbarkAction(__instance.PlayerId, worldCoordinates));
+                }
             }
         }
 
